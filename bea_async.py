4d1da18ce4cfba191da2fa76a0550e25b880b3f0
0-steve <steven.han19@gmail.com>
2024-03-05 23:31:20 -0500

    inital push

---
+++ bea_async.py
@@ -0,0 +1,127 @@
+import beaapi as bea
+import re
+import asyncio
+import aiohttp
+from functools import lru_cache
+
+class bea_api(): 
+    def __init__(self, api_key): 
+        """ 
+        api_key = api key needed for census api
+        """
+        self.key = api_key
+
+    def param_vals(self, dataset, param):
+        return bea.get_parameter_values(self.key, datasetname=dataset, parametername=param)
+
+    @lru_cache
+    def linecode_lookup(self):
+        """
+        Line codes represent different endpoints available from BEA.
+        """
+        linecodes = self.param_vals("Regional", "LineCode")
+        linecodes["table"] = linecodes.Desc.apply(lambda x: re.search("(?<=\[).+(?=\])", x).group(0))
+        linecodes["Desc"] = linecodes.Desc.apply(lambda x: x.split("] ")[1])
+        return linecodes
+
+    async def get_state_linecodes(self, table, line_code):
+        """
+        Call BEA API to get region wide data for the provided table and line code.
+        """
+        return bea.get_data(self.key, datasetname="Regional", TableName=table, LineCode=line_code, GeoFips="STATE", year="ALL")
+    
+    def get_bea_keys(self, table):
+        """
+        Collect all line codes for the given table. Some tables may require additional cleaning.
+        """
+        linecodes = self.linecode_lookup()
+        keys = linecodes[(linecodes.table==table)].drop_duplicates()
+        if table == "SAGDP4N":
+            return tuple(k for k in keys.Key.unique() if not re.search("\(", k)) # get keys for industries with a parenthesis
+        else:
+            return tuple(keys.Key)
+    
+    async def bea_compensation(self, bea_session):
+        """
+        Aysnc calls for every compensation line code
+        """
+        compensation_keys = self.get_bea_keys("SAGDP4N")
+        compensation_results = {}
+        async with bea_session as session:
+            print("Collecting BEA compensation data")
+            for k in compensation_keys:
+                key_results = await self.get_state_linecodes("SAGDP4N", k)
+                compensation_results[f"SAGDP4N-{k}"] = key_results
+                print("Compensation key found", k)
+            print("BEA compensation data collected")
+            print()
+            return compensation_results
+    
+    async def bea_consumption(self, bea_session):
+        """
+        Aysnc calls for every consumption line code
+        """
+        consumption_keys = self.get_bea_keys("SAPCE2")
+        consumption_results = {}
+        async with bea_session as session:
+            print("Collecting BEA consumption data")
+            for k in consumption_keys:
+                key_results = await self.get_state_linecodes("SAPCE2", k)
+                consumption_results[f"SAPCE2-{k}"] = key_results
+                print("Consumption key found", k)
+            print("BEA consumption data collected")
+            print()
+            return consumption_results
+
+    async def bea_personal_income(self, bea_session):
+        """
+        Aysnc calls for every personal income line code
+        """
+        personal_income_keys = self.get_bea_keys("SAINC30")
+        personal_income_results = {}
+        async with bea_session as session:
+            print("Collecting BEA personal income data")
+            for k in personal_income_keys:
+                key_results = await self.get_state_linecodes("SAINC30", k)
+                personal_income_results[f"SAINC30-{k}"] = key_results
+                print("Personal income key found", k)
+            print("BEA personal income data collected")
+            print()
+            return personal_income_results
+
+    async def bea_disposable_income(self, bea_session):
+        """
+        Aysnc calls for every disposable income line code
+        """
+        disposable_income_keys = self.get_bea_keys("SAINC51")
+        disposable_income_results = {}
+        async with bea_session as session:
+            print("Collecting BEA disposable income data")
+            for k in disposable_income_keys:
+                key_results = await self.get_state_linecodes("SAINC51", k)
+                disposable_income_results[f"SAINC51-{k}"] = key_results
+                print("Disposable income key found", k)
+            print("BEA disposable income data collected")
+            print()
+            return disposable_income_results
+
+    async def async_bea_api(self):
+        """
+        Run all async functions
+        """
+        async with aiohttp.ClientSession() as session:
+            task_results = await asyncio.gather(*(self.bea_compensation(session), 
+                                                  self.bea_consumption(session), 
+                                                  self.bea_personal_income(session),
+                                                  self.bea_disposable_income(session)))
+            return task_results
+    
+    def collect_data(self):
+        print("Collecting BEA data for compensation of employees, personal consumption expenditures, personal income, & disposable income")
+        print()
+        results = asyncio.run(self.async_bea_api()) 
+
+        print("BEA async complete")
+        print()
+        return results
+
---
+++ clean_bea_data.py
@@ -0,0 +1,82 @@
+import pandas as pd
+from functools import lru_cache
+import json
+import re
+from concurrent.futures import ThreadPoolExecutor
+import os
+
+from bea_async import bea_api
+
+
+class bea_data_clean():
+    """
+    Region wide data from BEA includes state level, country level, and regional level full names. 
+    Reference Wikipedia to collect state + country names (50 states + DC + US).
+    """
+    @lru_cache
+    def get_state_names(self):
+        wiki_url = "https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States"
+        wiki_states = pd.read_html(wiki_url)
+        dc = (wiki_states[2].iloc[0, 0],)
+        us = (re.search("United States", wiki_states[0][0][1]).group(0) + " *", ) # us code for country level data
+        states = tuple(state.replace("[B]", "") for state in wiki_states[1].droplevel(0, axis=1).iloc[:50, 0]) # remove annotations in name
+        return us + states + dc
+    
+    @lru_cache
+    def collect_bea_data(self, key):
+        """
+        Collect API data from bea_async library
+        """
+        async_api = bea_api(key)
+        results =  async_api.collect_data()
+        return results
+    
+    def state_filter(self, data_dict, bea_variable):
+        """
+        Filter data for region names desired from Wikipedia and write data as JSON files
+        """
+        df_joined = pd.concat(data_dict.values()).reset_index(drop=True)
+        df_joined_states = df_joined[df_joined.GeoName.isin(self.get_state_names())]
+        df_joined_states = df_joined_states.to_json()
+        file_path = os.path.join("data", f"{bea_variable}.json")
+        with open(file_path, "w") as outfile:
+            json.dump(df_joined_states, outfile)
+        outfile.close()
+
+    def write_files(self, bea_data_dict):
+        """
+        The function to be run in each of the pools. Reference the linecode in the data to know which BEA data is being used.
+        """
+        if "SAGDP4N-1" in bea_data_dict.keys():
+            print("Writing BEA compensation data as JSON")
+            bea_data.state_filter(bea_data_dict, "Compensation")
+        elif "SAPCE2-1" in bea_data_dict.keys():
+            print("Writing BEA consumption data as JSON")
+            bea_data.state_filter(bea_data_dict, "Consumption_expenditures")
+        elif "SAINC30-10" in bea_data_dict.keys():
+            print("Writing BEA personal income data as JSON")
+            bea_data.state_filter(bea_data_dict, "Personal_income")
+        elif "SAINC51-51" in bea_data_dict.keys():
+            print("Writing BEA disposable income data as JSON")
+            bea_data.state_filter(bea_data_dict, "Disposable_income")
+
+    def file_save_threads(self, bea_data):
+        """
+        Execute multiple pools for the thread to run write_file functions 
+        """
+        with ThreadPoolExecutor() as executor:
+            executor.map(self.write_files, bea_data)
+
+if __name__ == "__main__":
+    from datetime import datetime
+    
+    startTime = datetime.now()
+
+    key = os.environ.get("BEA_KEY")
+    bea_data = bea_data_clean()
+    data = bea_data.collect_bea_data(key)
+    bea_data.file_save_threads(data)
+
+    print("BEA data saved as JSON in data folder")
+    print()
+    print("Run time", datetime.now() - startTime)
